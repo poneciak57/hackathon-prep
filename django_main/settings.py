25c75d58b1972e9b0c3e0d6cd759e1bb8f5e5951
SomePersonFromMars <42392219+SomePersonFromMars@users.noreply.github.com>
2024-11-10 12:52:31 +0100

    Finished part 2 of the Django tutorial

--- django_main/settings.py
+++ django_main/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -110,7 +111,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Warsaw'
 
 USE_I18N = True
 
