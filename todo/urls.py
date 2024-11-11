a123346d661d6739ab0adaa785d513add14e7562
kubat1234 <kubateq@gmail.com>
2024-11-11 12:52:12 +0100

    added todo list

---
+++ todo/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('add/', views.add_todo, name='add_todo'),
+    path('delete/<int:todo_id>/', views.delete_todo, name='delete_todo'),
+]
--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import TodoItem
 
-# Create your views here.
+def index(request):
+    todo_items = TodoItem.objects.all()
+    return render(request, 'todo/index.html', {'todo_items': todo_items})
+
+def add_todo(request):
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        if title:
+            TodoItem.objects.create(title=title)
+    return redirect('index')
+
+def delete_todo(request, todo_id):
+    TodoItem.objects.filter(id=todo_id).delete()
+    return redirect('index')
